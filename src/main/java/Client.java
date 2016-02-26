5953aba9822fbda16714a18ffa11d365429a39f0
chalmie <blairchalmers@gmail.com>
2016-02-26 08:42:11 -0800

    create databases and link to project

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,5 +2,53 @@ import org.sql2o.*;
 import java.util.List;
 
 public class Client {
+  private int id;
+  private String name;
+  private int stylist_id;
 
+  public Client (String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getStylistId() {
+    return stylist_id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public boolean equals(Object otherClient){
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+        this.getId() == newClient.getId();
+    }
+  }
+
+  //CREATE
+  public void save() {
+    try (Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO clients(name) VALUES (:name)";
+    this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", name)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  //READ
+  public static List<Client> all() {
+    String sql = "SELECT * FROM clients";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
 }
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -7,9 +7,5 @@ public class StylistTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
-  @Test
-  public void all_emptyAtFirst() {
-      assertEquals(Stylist.all().size(), 0);
-  }
 
 }
